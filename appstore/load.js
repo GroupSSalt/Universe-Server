05fa012f995ba989e82f0dbb020067811abbc837
GroupSSalt <148018272+GroupSSalt@users.noreply.github.com>
2024-03-19 06:48:59 -0600

    Update load.js

--- appstore/load.js
+++ appstore/load.js
@@ -8,12 +8,12 @@ function loadAppstore() {
 		</div>
 		<div class="tabcontent" id="appstore-home">
 		<h1>Universe Appstore</h1>
-		<button class="padIcon" onClick="openTab('appstore-clicker')"><img class="padImage" src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png"></button>
+		<button class="padIcon" onClick="openTab('appstore-clicker')"><img class="padImage" src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png?raw=true"></button>
 	</div>
 		<div class="tabcontent" id="appstore-clicker">
 	<center>
 			<h1>Universe Clicker</h1>
-			<img src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png"><br>
+			<img src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png?raw=true"><br>
 			<span>By: GroupSSalt</span><br>
 			<span>Version: 1</span><br><br>
 			<button class="button" onClick="installClicker()" id="installClicker">Install</button>
