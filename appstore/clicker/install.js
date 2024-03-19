a6cc2b6700aaef38193d5a4f3f931f75dd591b14
GroupSSalt <148018272+GroupSSalt@users.noreply.github.com>
2024-03-19 06:47:19 -0600

    Update install.js

--- appstore/clicker/install.js
+++ appstore/clicker/install.js
@@ -22,12 +22,12 @@ function installClicker() {
     let deskpad = document.getElementById('deskpad');
     deskpad.innerHTML += `
         <button class="padIcon" onClick="openApp('clicker')">
-            <img src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png" class="padImage">
+            <img src="https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png?raw=true" class="padImage">
         </button>
     `;
 
 	let script = document.createElement('script');
-    script.src = "https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker/program/clicker.js";
+    script.src = "https://github.com/GroupSSalt/Universe-Server/blob/main/appstore/clicker/clicker.png?raw=true";
     document.body.appendChild(script);
 
     document.getElementById('installClicker').innerHTML = "Installed";
